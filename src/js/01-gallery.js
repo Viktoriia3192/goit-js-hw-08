94f4726c7bf766080e835bbdbfd21b0028ae9038
Viktoriia3192 <vikysja9231@gmail.com>
2023-08-22 21:07:36 +0100

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,59 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+function renderGallery() {
+  const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+  galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+}
+
+function openModal(url) {
+  const instance = new SimpleLightbox(`<img src="${url}" alt="Image">`, {
+    onShow: (instance) => {
+      document.body.classList.add('modal-open');
+      instance.element().querySelector('img').focus();
+    },
+    onClose: () => {
+      document.body.classList.remove('modal-open');
+    },
+  });
+
+  instance.show();
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.classList.contains('gallery__image')) {
+    const largeImageUrl = target.dataset.source;
+    openModal(largeImageUrl);
+  }
+}
+
+galleryList.addEventListener('click', handleGalleryClick);
+
+renderGallery();
